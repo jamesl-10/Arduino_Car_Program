52366fc7b18b4306b2de5858bbe30c39e899baa8
James-Lee1 <james.lee@ocsbstudent.ca>
2019-11-17 18:31:20 -0500

    Update car.c

--- car.c
+++ car.c
@@ -1,12 +1,17 @@
- // Define pins for sensor
-#include <Servo.h>
+// Created by: James Lee
+// Teacher: Mr. Pewa
+// Course: TEJ3M
+// Code for automated Arduino car
+// Allows car to solve maze
 
+// Declare Servo
+// Servo can only be declared on pins 9/10
+#include <Servo.h>
 Servo servo_9;
 
+// Pins for ultrasound sensor
 #define trigPin 10
 #define echoPin 13
-
-float duration, distance;
 
 // Define pins for wheels
 const int IN1 = 7;
@@ -14,28 +19,28 @@ const int IN2 = 6;
 const int IN3 = 5;
 const int IN4 = 4;
 
+// Pins that control motor speed
 const int ENA = 3; // pins for motor speed control for both
-
 const int ENB = 8;
 
-String direction = "Forward";
+// Variables
+float duration, distance;
+int Forward = 70;
+int Left = 160;
+int Right = -20;
 
 // Initial function
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  
   Serial.begin (9600);
+  delay(1000);
   
   servo_9.attach(9);
   //160 is left
   //70 is forward
   //-20 is right
-  servo_9.write(70);
+  servo_9.write(Forward);
   
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -46,7 +51,7 @@ void setup()
 }
 
 // Turns left
-// Maybe make left turn faster than right
+// Maybe make left turn faster than right?
 void turnLeft()
 {
     digitalWrite(IN1, HIGH);
@@ -55,6 +60,9 @@ void turnLeft()
     digitalWrite(IN4, LOW);
     
     delay(1000);
+    
+    analogWrite(ENA, 0);
+    analogWrite(ENB, 0);
 }
 
 // Turns right
@@ -66,8 +74,12 @@ void turnRight()
   digitalWrite(IN4, HIGH);
 
   delay(1000);
+  
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
 }
 
+// Goes forward
 void goForward()
 {
   digitalWrite(IN1, HIGH);
@@ -90,29 +102,14 @@ void stopCar()
   analogWrite(ENB, 0);
 }
 
-void lookLeft()
+void look(int direction)
 {
-  servo_9.write(160);
+  servo_9.write(direction);
 }
 
-void lookRight()
+int findDistance()
 {
-  servo_9.write(-20);
-}
-
-void lookForward()
-{
-  servo_9.write(70);
-}
-
-void loop()
-{
-  // put your main code here, to run repeatedly:
-  
   // Code for finding distance in front of sensor
-  analogWrite(ENA, 200);
-  analogWrite(ENB, 230);
-  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -122,42 +119,56 @@ void loop()
   duration = pulseIn(echoPin, HIGH);
 
   distance = (duration / 2) * 0.0343;
+  
+  return distance;
+}
 
-  if (distance >= 15)
+// put your main code here, to run repeatedly:
+void loop()
+{
+  // Controls speed of motors
+  analogWrite(ENA, 150);
+  analogWrite(ENB, 150);
+
+  if (findDistance() >= 15) // When there is no obstable in front of car
   {
-    if (direction == "Right")
+    look(Forward);
+    goForward();
+  }
+  
+  else //When there is an obstacle in front of car
+  {
+    stopCar();
+
+    look(Left);
+    delay(1500);
+    int distanceLeft = findDistance();
+    Serial.println(findDistance());
+    
+    look(Right);
+    delay(1500);
+    int distanceRight = findDistance();
+    Serial.println(findDistance());
+
+    if (distanceRight < 15 && distanceLeft < 15)
+    {
+      // Turn 180
+      goBackward();
+    }
+    else if (distanceRight > distanceLeft)
     {
       turnRight();
     }
-    if (direction == "Left")
+    else if (distanceRight < distanceLeft)
     {
       turnLeft();
     }
-    lookForward();
-    direction = "Forward";
-    goForward();   
-  }
-  
-  else
-  {
-    stopCar();
+    else if (distanceRight == distanceLeft)
+    {
+      turnLeft();
+    }
     
-    if (direction == "Forward")
-    {
-      lookRight();
-      direction = "Right";
-    }
-    else if (direction == "Right")
-    {
-      lookLeft();
-      direction = "Left";
-    }
-    else
-    {
-      goBackward();
-      Serial.print("Go back");
-    }
-
+    look(Forward);
     delay(1000);
   }
 }
